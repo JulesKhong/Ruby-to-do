421d012c271af1e43bbf2a0c2b5aba9ea8ab1a51
Julia Dickey and James Turello <james@>
2016-07-11 10:45:58 -0700

    Added a whole bunch: connect to database with lists and tasks, pass all specs

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,33 +1,40 @@
-require('to_do')
-require('rspec')
+require('spec_helper')
 
 describe(Task) do
-  before() do
-    Task.clear()
-  end
-  describe('#description') do
-    it('lets you give a description') do
-      test_task = Task.new("scrub the zebra")
-      expect(test_task.description()).to (eq("scrub the zebra"))
-    end
-  end
   describe(".all") do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
     end
   end
+
   describe("#save") do
     it("adds a task to an array of saved tasks") do
-      test_task = Task.new("wash the lion")
+      test_task = Task.new({:description => "scrub the zebra", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      Task.new("wash the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
+
+  describe('#description') do
+    it('lets you give a description') do
+      test_task = Task.new({:description => "scrub the zebra", :list_id => 1})
+      expect(test_task.description()).to (eq("scrub the zebra"))
     end
   end
+
+  describe("#list_id") do
+    it("lets your read the list ID out") do
+      test_task = Task.new({:description => "scrub the zebra", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "scrub the zebra", :list_id => 1})
+      task2 = Task.new({:description => "scrub the zebra", :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
+  end
+
 end
